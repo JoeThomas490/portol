26064a68fb8c50a594ef081b36957e88fdf74ecd
JoeThomas490 <joe_thomas490@hotmail.com>
2026-07-10 14:30:21 +0100

    Revert "Fixed build error"
    This reverts commit 6b1cb1511e19eaa52db65a2af15c786c6d3dca6e.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   integrations: [react()],
-  image: {
-    domains: ['assets.joenoseph.co.uk', 'cdn.simpleicons.org'],
-  },
   vite: {
     plugins: [tailwindcss()],
   },
